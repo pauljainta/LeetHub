6f09e6aec4b8e210c5904635e93259b77ec450f2
pauljainta <58131098+pauljainta@users.noreply.github.com>
2021-05-03 23:10:11 +0600

    Longest pallindromic substring accepted

--- 5.longest-palindromic-substring.cpp
+++ 5.longest-palindromic-substring.cpp
@@ -36,14 +36,14 @@ public:
         for(int i=0;i<s.length();i++)
         {
             arr[i][i]=1;
-            if(i!==s.length()-1)
+            if(i!=s.length()-1)
             {
                 if(s[i]==s[i+1])
                 {
                     arr[i][i+1]=2;
                     longestPall=s.substr(i,2);
                 }
-                else arr[i][i+1]=0;
+               
             }
         }
 
@@ -64,47 +64,7 @@ public:
         }
 
         return longestPall;
-        
-        // for(int i=0;i<s.length()-1;i++)
-        // {
-        //     // for(int j=2;j<s.length()-i;j++)
-        //     // {
-        //     //     if(s[i]==s[i+j] && arr[i+1][j-1]!=0)
-        //     //     {
-        //     //         arr[i][i+j]=arr[i+1][j-1]+j
-        //     //     }
-        //     // }
-        //     if(s[i]==s[i+j] && arr[i+1][j-1]!=0)
-        //     {
-        //         arr[i][i+j]=arr[i+1][j-1]+2;
-        //     }
-
-
-
-        // }
-
-       
-        
-
-
-        // string longestPall=s.substr(0,1);
     
-        // int maxLength = 1;
- 
- 
-        // for (int i = 0; i < s.length(); i++) {
-        //     for (int j = i; j < s.length(); j++) {
-            
-        //         if (CheckPallindrome(i,j,s) && (j - i + 1) > maxLength) {
-        //             //start = i;
-        //             maxLength = j - i + 1;
-        //             longestPall=s.substr(i,maxLength);
-
-        //         }
-        //     }
-        // }
- 
-        // return longestPall;
         
     }
 };
