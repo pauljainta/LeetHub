e04491d748ba4d4787b68dbf669f5149baaf380a
pauljainta <58131098+pauljainta@users.noreply.github.com>
2021-05-04 22:24:15 +0600

    container with most water accepted

--- 11.container-with-most-water.cpp
+++ 11.container-with-most-water.cpp
@@ -11,20 +11,49 @@ public:
     {
         return abs(index_1-index_2)*height;
     }
+    int min(int a ,int b)
+    {
+        if(a>=b) return b;
+        return a;
+
+    }
     int maxArea(vector<int>& height) 
     {
-        int max=0;
-        for(int i=0;i<height->size();i++)
+        int maxA=AreaBetween(0,height.size()-1,min(height.at(0),height.at(height.size()-1)));
+
+        int startPointer=0,endPointer=height.size()-1;
+
+        while(startPointer<endPointer)
         {
-            for(int j=0;j<height->size();j++)
+            if(min(height.at(startPointer),height.at(endPointer))==height.at(startPointer))
             {
-                if(i!=j && height->at(j)>=height->at(i) && AreaBetween(i,j,height->at(i)>max)
-                {
-                    max=AreaBetween(i,j,height->at(i);
-                }
+                startPointer++;
+                
+            }
+            else
+            {
+                endPointer--;
+
+            }
+
+            if(AreaBetween(startPointer,endPointer,min(height.at(startPointer),height.at(endPointer)))>maxA)
+            {
+                maxA=AreaBetween(startPointer,endPointer,min(height.at(startPointer),height.at(endPointer)));
             }
         }
-        return max;
+
+
+        // for(int i=0;i<height.size();i++)
+        // {
+        //     for(int j=0;j<height.size();j++)
+        //     {
+        //         if(i!=j && height.at(j)>=height.at(i) && AreaBetween(i,j,height.at(i))>max)
+        //         {
+        //             max=AreaBetween(i,j,height.at(i));
+        //         }
+        //     }
+        // }
+        return maxA;
     }
 };
 // @lc code=end
