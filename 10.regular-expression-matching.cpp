0b8713d7c060bb3f5da70330b6d14ed80cb217ef
pauljainta <58131098+pauljainta@users.noreply.github.com>
2021-05-04 12:37:59 +0600

    Regular epression matching accepted

---
+++ 10.regular-expression-matching.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=10 lang=cpp
+ *
+ * [10] Regular Expression Matching
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        // int l=s.length();
+        // if(s[l-2]=="." && s[l-1]=="*") return true;
+        if (regex_match (s, regex(p)) return true;
+
+        return false;
+        
+
+        
+    }
+};
+// @lc code=end
+
