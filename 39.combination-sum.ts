2a2ffedca4be0c58459fabf005d860961fa59562
pauljainta <58131098+pauljainta@users.noreply.github.com>
2021-05-16 10:45:26 +0600

    41 accepted

---
+++ 39.combination-sum.ts
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=39 lang=typescript
+ *
+ * [39] Combination Sum
+ */
+
+// @lc code=start
+
+let combinations:number[][];
+
+function combinationSum(candidates: number[], target: number): number[][] {
+    combinations=[];
+
+
+
+};
+// @lc code=end
+
---
+++ 41.first-missing-positive.ts
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=41 lang=typescript
+ *
+ * [41] First Missing Positive
+ */
+
+// @lc code=start
+function firstMissingPositive(nums: number[]): number {
+
+    let i;
+  
+    for(i=0;i<nums.length;i++){
+       let correctPos=nums[i]-1;
+       while(nums[i]>=1 && nums[i]<=nums.length && nums[i]!=nums[correctPos])
+       {
+            let temp=nums[i];
+            nums[i]=nums[correctPos];
+            nums[correctPos]=temp;
+            
+            correctPos=nums[i]-1;
+       }
+    }
+
+    return i;
+
+
+
+    
+
+
+
+
+
+
+};
+// @lc code=end
+
