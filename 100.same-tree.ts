4bec5ce40cbe563eef3abc3ffe69c3e695b52062
pauljainta <58131098+pauljainta@users.noreply.github.com>
2021-05-17 12:33:25 +0600

    100 accepted

---
+++ 100.same-tree.ts
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=100 lang=typescript
+ *
+ * [100] Same Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+    if(p==null && q==null) return true;
+    else if(p==null) return false;
+    else if(q==null) return false;
+    else if(p.val!=q.val) return false;
+    else
+    {
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right)
+    }
+
+    return true;
+
+
+};
+// @lc code=end
+
