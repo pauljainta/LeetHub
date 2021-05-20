587d25ee151bed8e142f5d804d55edce26c55937
pauljainta <58131098+pauljainta@users.noreply.github.com>
2021-05-20 08:52:37 +0600

    122 accepted

--- 122.best-time-to-buy-and-sell-stock-ii.ts
+++ 122.best-time-to-buy-and-sell-stock-ii.ts
@@ -7,13 +7,9 @@
 // @lc code=start
 function maxProfit(prices: number[]): number {
     let max_profit=0;
-    for(let i=0;i<prices.length-1;i++)
+    for(let i=1;i<prices.length;i++)
     {
-        if(i!=0 && prices[i]>=prices[i-1] ) continue;
-        for(let j=i+1;j<prices.length;j++)
-        {
-            if(prices[j]-prices[i]>max_profit) max_profit=prices[j]-prices[i];
-        }
+        if(prices[i]>prices[i-1]) max_profit+=(prices[i]-prices[i-1]);
     }
 
     return max_profit;
